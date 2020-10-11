073ee113f92e1f21195b860ca9e08692443fbe4f
Rohin1212 <rohinchopra1212@gmail.com>
2020-10-12 00:43:38 +1100

    indentation error fix

--- app/scraper.py
+++ app/scraper.py
@@ -9,7 +9,7 @@ class DoubtfireScraper:
         self.url = "https://doubtfire.ict.swin.edu.au"
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--headless")
-    	chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.binary_location = os.environ["GOOGLE_CHROME_BIN"]
         self.driver = webdriver.Chrome(executable_path=os.environ["CHROMEDRIVER_PATH"], chrome_options=chrome_options)
