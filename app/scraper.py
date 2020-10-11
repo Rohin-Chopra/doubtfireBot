76bddf1751d1298e0a7a3dd411d6729ff00d4fc0
Rohin1212 <rohinchopra1212@gmail.com>
2020-10-12 00:28:57 +1100

    changes to chrome driver

--- app/scraper.py
+++ app/scraper.py
@@ -10,7 +10,7 @@ class DoubtfireScraper:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
-        chrome_options.binary_location = os.environ["GOOGLE_CHROME_PATH"]
+        chrome_options.binary_location = os.environ["GOOGLE_CHROME_BIN"]
         self.driver = webdriver.Chrome(execution_path=os.environ["CHROMEDRIVER_PATH"], chrome_options=chrome_options)
 
     def refresh(self):
