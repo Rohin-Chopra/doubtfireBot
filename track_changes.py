628950e07d2ba7b20097dea38f39d1b7605fcbd7
Rohin1212 <rohinchopra1212@gmail.com>
2020-10-12 11:24:41 +1100

    final

--- track_changes.py
+++ track_changes.py
@@ -19,17 +19,5 @@ units_with_tasks = ds.find_units_tasks(units)
 
 dtc = DoubtfireTrackChanges(cursor)
 
-# dtc.check_task_status_changed(units_with_tasks)
+dtc.check_task_status_changed(units_with_tasks)
 
-
-first_scan_time = dt.datetime.now() + dt.timedelta(
-    minutes=2
-)  # set your sending time in UTC
-interval = dt.timedelta(minutes=2)  # set the interval for sending the email
-
-scan_time = first_scan_time
-dtc = DoubtfireTrackChanges(cursor)
-
-while True:
-    dtc.check_task_status_changed_at(partial(ds.find_units_tasks,units), scan_time)
-    scan_time += interval
