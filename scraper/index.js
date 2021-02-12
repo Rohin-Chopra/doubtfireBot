58bf799af9d2876cbaebc6b620f69c0199bf0cbe
Rohin1212 <rohinchopra1212@gmail.com>
2021-02-12 12:21:46 +1100

    feat: scrapes through doubtfire website

--- scraper/index.js
+++ scraper/index.js
@@ -1,15 +1,5 @@
-const dotenv = require("dotenv");
-dotenv.config({});
+const dotenv = require('dotenv')
+dotenv.config({})
+const scrape = require('./scrape')
 
-const sequelize = require("./../sequelize");
-
-const scrape = require("./scrape");
-
-sequelize
-  .authenticate()
-  .then(() => console.log("database connected"))
-  .catch((err) => console.error(err));
-sequelize.sync();
-
-scrape(sequelize);
-// list-group-item-heading ng-binding
+module.exports = scrape
--- scraper/scrape.js
+++ scraper/scrape.js
@@ -1,73 +1,176 @@
-const puppeteer = require("puppeteer");
-const cheerio = require("cheerio");
-const Cryptr = require("cryptr");
+const fs = require('fs')
+const path = require('path')
+const puppeteer = require('puppeteer')
+const cheerio = require('cheerio')
+const sendEmail = require('./email')
 
 module.exports = async (sequelize) => {
   try {
-    let $;
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto("https://doubtfire.ict.swin.edu.au/#/home");
-    console.log("page loaded");
+    let $
+    const browser = await puppeteer.launch({ headless: true })
+    const page = await browser.newPage()
+    await page.goto('https://doubtfire.ict.swin.edu.au/#/home')
 
-    const students = await sequelize.models.Student.findAll();
+    const users = await sequelize.User.findAll({
+      include: sequelize.Unit
+    })
 
-    students.forEach(async (student) => {
-      const s = student.dataValues;
+    for await (const u of users) {
+      const user = u.dataValues
+      login(user.id, u.decryptStudentPassword(), page)
 
-      login(s.studentId, s.studentPassword, page);
-      await page.waitForNavigation();
-      await page.waitForSelector(".list-group-item");
+      await page.waitForNavigation()
+      await page.waitForSelector('.list-group-item')
 
-      $ = cheerio.load(await page.content());
+      $ = cheerio.load(await page.content())
 
-      const units = loadUnits($, sequelize);
-      saveUnits(units, null, sequelize);
-      const tasks = loadTasks($, sequelize);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-// list-group-item-heading ng-binding
+      const loadedUnits = loadUnits($)
+      const units = await saveUnits(loadedUnits, u, sequelize)
+
+      const tasks = await loadTasks(units, $, page)
+      saveTasks(tasks, u, sequelize)
+    }
+    await browser.close()
+    const { green } = require('chalk')
+    console.log(green('done scraping'))
+  } catch (error) {}
+}
 
 const login = async (username, password, page) => {
-  const cryptr = new Cryptr(process.env.SALT);
-  await page.type("input[type=username]", username);
-  await page.type("input[type=password]", cryptr.decrypt(password));
-  await page.click("button[type=submit]");
-};
+  await page.type('input[type=username]', username)
+  await page.type('input[type=password]', password)
+  await page.click('button[type=submit]')
+}
 
 const loadUnits = ($) => {
-  const units = [];
-  $(".list-group-item-heading", ".list-group-item")
+  const units = []
+  $('.list-group-item-heading', '.list-group-item')
     .toArray()
     .forEach((h4) => {
       units.push({
         name: h4.children[0].data,
-        code: "",
-        link: h4.parent.parent.attribs.href,
-      });
-    });
+        code: '',
+        link: h4.parent.parent.attribs.href
+      })
+    })
 
-  $(".label-info")
+  $('.label-info')
     .toArray()
     .forEach((label, index) => {
       if (index < units.length) {
-        units[index].code = label.children[0].data;
+        units[index].code = label.children[0].data
       }
-    });
-  return units;
-};
+    })
+  return units
+}
 
-const saveUnits = (units, studentId, sequelize) => {
-  units.forEach(async (unit) => {
-    if (!(await sequelize.models.Unit.findByPk(unit.code))) {
-      await sequelize.models.Unit.create(unit);
+const saveUnits = async (loadedUnits, user, sequelize) => {
+  const savedUnits = []
+  for await (const unit of user.Units) {
+    loadedUnits.forEach(async (u) => {
+      if (u.code === unit.dataValues.code) {
+        // if it already exists in Unit and UserUnits
+        savedUnits.push(unit)
+      } else if (
+        !(await sequelize.UserUnit.findOne({
+          where: {
+            unit_code: u.code,
+            user_id: user.dataValues.id
+          }
+        }))
+      ) {
+        const createdUnit = await sequelize.Unit.create(unit)
+        savedUnits.push(createdUnit)
+        await createdUnit.addUser(user)
+      }
+    })
+  }
+
+  return savedUnits
+}
+
+const loadTasks = async (units, $, page) => {
+  const tasks = []
+  for await (const u of units) {
+    const unit = u.dataValues
+    await page.goto(`https://doubtfire.ict.swin.edu.au/${unit.link}`)
+    await page.waitForSelector('.project-tasks-list')
+    $ = cheerio.load(await page.content())
+    const a = $('button', '.project-tasks-list')
+    Object.keys(a).forEach((key) => {
+      try {
+        const element = a[key]
+        tasks.push({
+          name: element.children[2].data.trim(),
+          status: element.attribs.class.split(' ')[3],
+          unit: u
+        })
+      } catch (error) {
+        if (error.name !== 'TypeError') {
+          throw error
+        }
+      }
+    })
+  }
+  return tasks
+}
+
+const saveTasks = (tasks, user, sequelize) => {
+  tasks.forEach(async (task) => {
+    try {
+      const existingTask = await sequelize.Task.findOne({
+        where: { name: task.name }
+      })
+      const existingTaskStatus = (
+        await sequelize.UserTask.findOne({
+          where: {
+            task_name: task.name,
+            user_id: user.dataValues.id
+          }
+        })
+      )?.dataValues?.status
+
+      if (!existingTask) {
+        const createdTask = await sequelize.Task.create(task)
+        await createdTask.addUnit(task.unit, { status: task.status })
+        await createdTask.addUser(user)
+      } else if (!(await existingTask.hasUser(user))) {
+        await existingTask.addUser(user)
+      } else if (existingTaskStatus !== task.status) {
+        console.log('sending email')
+        fs.readFile(
+          path.join(__dirname, 'data/taskStatusChanged.txt'),
+          'utf-8',
+          (err, data) => {
+            if (err) {
+              console.log(err)
+            }
+            const message = data
+              .replace('{TASKNAME}', task.name)
+              .replace('{UNIT_NAME}', task.unit.code)
+              .replace('{NAME}', user.dataValues.first_name)
+              .replace('{TASK_STATUS}', task.status)
+            sendEmail({
+              from: 'rohinpython@gmail.com',
+              to: user.dataValues.email,
+              subject: `Task ${task.name}'s status changed to ${task.status}`,
+              text: message
+            })
+          }
+        )
+
+        await sequelize.UserTask.update(
+          { status: task.status },
+          {
+            where: {
+              task_name: task.name,
+              user_id: user.dataValues.id
+            }
+          }
+        )
+      }
+    } catch (error) {
+      console.log(error)
     }
-  });
-};
-
-const loadTasks = ($, sequelize) => {
-  return null;
-};
+  })
+}
