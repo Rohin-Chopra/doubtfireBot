35d707416125cc2a114831357721210b715e1b86
Rohin1212 <rohinchopra1212@gmail.com>
2021-02-13 23:01:28 +1100

    updated routes

--- server/app.js
+++ server/app.js
@@ -1,17 +1,27 @@
-const express = require("express");
+const express = require('express')
 
-const app = express();
-const studentRouter = require("./routes/student");
-const errorHandler = require("./middlewares/error");
+const app = express()
+const authRouter = require('./routes/auth')
+const userRouter = require('./routes/user')
+const errorHandler = require('./middlewares/error')
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/student", studentRouter);
+app.use('/', authRouter)
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+app.use('/users', userRouter)
 
-app.use(errorHandler);
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
 
-module.exports = app;
+app.use(errorHandler)
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: 'route not found on this server'
+  })
+})
+
+module.exports = app
