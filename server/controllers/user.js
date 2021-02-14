98c8910dedd29823ae221782ac345e9a4eb4137b
Rohin1212 <rohinchopra1212@gmail.com>
2021-02-14 12:16:20 +1100

    FEAT: added U&D operations to user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,11 +4,52 @@ const { User } = sequelize
 
 exports.getUser = asyncHandler(async (req, res, next) => {
   const user = await User.scope('excludePassword').findByPk(req.user.id)
-
+  if (!user) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'User not found for id '
+    })
+  }
   res.status(200).json({
     status: 'success',
     data: {
       user
     }
+  })
+})
+
+exports.updateUser = asyncHandler(async (req, res, next) => {
+  await User.update(
+    {
+      first_name: req.body.first_name || req.user.first_name,
+      last_name: req.body.last_name || req.user.last_name,
+      email: req.body.email || req.user.email
+    },
+    {
+      where: {
+        id: req.user.id
+      }
+    }
+  )
+  const updatedUser = await User.scope('excludePassword').findByPk(req.user.id)
+  console.log(updatedUser)
+  res.status(200).json({
+    status: 'success',
+    message: 'User updated',
+    data: {
+      user: updatedUser
+    }
+  })
+})
+
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+  await User.destroy({
+    where: {
+      id: req.user.id
+    }
+  })
+  res.status(200).json({
+    status: 'success',
+    message: 'User deleted'
   })
 })
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,13 @@
 const { Router } = require('express')
-const { getUser } = require('../controllers/user')
+const { getUser, updateUser, deleteUser } = require('../controllers/user')
 const { protect, signUp } = require('../controllers/auth')
 
 const router = new Router()
 
 router.route('/').post(signUp)
-router.route('/:id').get(protect, getUser)
+
+router.use(protect)
+
+router.route('/:id').get(getUser).put(updateUser).delete(deleteUser)
 
 module.exports = router
