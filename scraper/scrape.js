4e77372ea236e4f6243c29ce38db4374f0582bcc
Rohin1212 <rohinchopra1212@gmail.com>
2021-02-17 23:42:05 +1100

    FEAT :  added notifier of task due date 1 day prior

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -3,6 +3,7 @@ const path = require('path')
 const puppeteer = require('puppeteer')
 const cheerio = require('cheerio')
 const sendEmail = require('./../utils/email')
+const delay = require('./../utils/delay')
 
 module.exports = async (sequelize) => {
   try {
@@ -26,15 +27,15 @@ module.exports = async (sequelize) => {
 
       const loadedUnits = loadUnits($)
       const units = await saveUnits(loadedUnits, u, sequelize)
-      const tasks = await loadTasks(units, $, page)
+      const tasks = await loadTasks(units, $, page, browser)
       saveTasks(tasks, u, sequelize)
       i += 1
       if (i === users.length) {
-        await browser.close()
-        const { green } = require('chalk')
-        console.log(green('done scraping'))
       }
     }
+    await browser.close()
+    const { green } = require('chalk')
+    console.log(green('done scraping'))
   } catch (error) {}
 }
 
@@ -92,29 +93,57 @@ const saveUnits = async (loadedUnits, user, sequelize) => {
   return savedUnits
 }
 
-const loadTasks = async (units, $, page) => {
+const loadTasks = async (units, $, page, browser) => {
   const tasks = []
   for await (const u of units) {
     const unit = u.dataValues
     await page.goto(`https://doubtfire.ict.swin.edu.au/${unit.link}`)
     await page.waitForSelector('.project-tasks-list')
     $ = cheerio.load(await page.content())
-    const a = $('button', '.project-tasks-list')
-    Object.keys(a).forEach((key) => {
+
+    const handles = await page.$$('.btn.task-status')
+
+    let index = 0
+    for (const handle of handles) {
       try {
-        const element = a[key]
-        tasks.push({
-          name: element.children[2].data.trim(),
-          status: element.attribs.class.split(' ')[3],
+        const task = {
+          name: (await page.evaluate((hl) => hl.textContent, handle)).trim(),
+          status: await page.evaluate((hl) => hl.classList[3], handle),
           unit: u
+        }
+        await page.evaluate((index) => {
+          const elements = [...document.querySelectorAll('.btn.task-status')]
+          return elements[index].click()
+        }, index)
+
+        await page.waitForSelector('.task-viewer-toolbar')
+        $ = cheerio.load(await page.content())
+        task.dueDate = new Date(
+          $('.label.label-warrning.ng-scope .ng-binding')
+            .text()
+            .trim()
+            .replace(/Deadline/g, '')
+            .trim()
+        )
+        await page.evaluate(() => {
+          return document
+            .querySelector('.nav.nav-tabs.nav-justified')
+            .firstElementChild.click()
         })
+        if (handles[0] === handle) {
+          task.dueDate = new Date()
+        }
+        tasks.push(task)
+        index++
       } catch (error) {
+        console.log(error)
         if (error.name !== 'TypeError') {
           throw error
         }
       }
-    })
+    }
   }
+
   return tasks
 }
 
@@ -132,15 +161,20 @@ const saveTasks = (tasks, user, sequelize) => {
           }
         })
       )?.dataValues?.status
+      const { red, blue } = require('chalk')
 
       if (!existingTask) {
+        console.log(blue('does not have any task and i am inserting'))
+
         const createdTask = await sequelize.Task.create(task)
         await createdTask.addUnit(task.unit, { status: task.status })
         await createdTask.addUser(user)
       } else if (!(await existingTask.hasUser(user))) {
+        console.log(red('does not have any user and i am inserting'))
+        console.log(await existingTask.hasUser(user))
+
         await existingTask.addUser(user)
       } else if (existingTaskStatus !== task.status) {
-        console.log('sending email')
         fs.readFile(
           path.join(__dirname, 'data/taskStatusChanged.txt'),
           'utf-8',
@@ -172,8 +206,35 @@ const saveTasks = (tasks, user, sequelize) => {
           }
         )
       }
-    } catch (error) {
-      console.log(error)
-    }
+      const tomorrow = new Date()
+      tomorrow.setDate(tomorrow.getDate() + 1)
+      if (Math.round((tomorrow - task.dueDate) / (1000 * 60 * 60 * 24)) === 1) {
+        fs.readFile(
+          path.join(__dirname, 'data/taskDueDateNotfication.txt'),
+          'utf-8',
+          (err, data) => {
+            if (err) {
+              console.log(err)
+            }
+            const message = data
+              .replace('{TASKNAME}', task.name)
+              .replace('{UNIT_NAME}', task.unit.code)
+              .replace('{NAME}', user.dataValues.first_name)
+              .replace(
+                '{TASK_DUE_DATE}',
+                `${task.dueDate.getDate()}/${
+                  task.dueDate.getMonth() + 1
+                }/${task.dueDate.getFullYear()}`
+              )
+            sendEmail({
+              from: 'rohinpython@gmail.com',
+              to: user.dataValues.email,
+              subject: `Task ${task.name}'s due date in 1 day`,
+              text: message
+            })
+          }
+        )
+      }
+    } catch (error) {}
   })
 }
