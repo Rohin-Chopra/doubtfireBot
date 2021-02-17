182b46ed10c23c2b9dbba13da881f2c3f217a15b
Rohin1212 <rohinchopra1212@gmail.com>
2021-02-18 00:44:49 +1100

    FEAT :  added task and unit router

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const authRouter = require('./routes/auth')
 const userRouter = require('./routes/user')
+const taskRouter = require('./routes/task')
+const unitRouter = require('./routes/unit')
 const errorHandler = require('./middlewares/error')
 
 app.use(express.json())
@@ -10,6 +12,10 @@ app.use(express.json())
 app.use('/', authRouter)
 
 app.use('/users', userRouter)
+
+app.use('/tasks', taskRouter)
+
+app.use('/units', unitRouter)
 
 app.get('/', (req, res) => {
   res.send('hello world')
