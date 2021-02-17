e262f48a5c77a5565d2a9288d2dcac9e237f91cd
Rohin1212 <rohinchopra1212@gmail.com>
2021-02-18 00:11:46 +1100

    REFACTOR : cleanup

--- server/server.js
+++ server/server.js
@@ -1,22 +1,20 @@
-const dotenv = require('dotenv')
-dotenv.config()
+require('dotenv').config()
+const { blue, yellow } = require('chalk')
+const cron = require('node-cron')
 
 const sequelize = require('./../sequelize/models')
-
 sequelize.start()
 
 const app = require('./app')
-const cron = require('node-cron')
 const scrape = require('./../scraper')
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  const { blue } = require('chalk')
   console.log(blue(`listening on port ${port}`))
 })
 
 cron.schedule('* * * * *', async () => {
-  console.log('about to scrape')
+  console.log(yellow('about to scrape'))
   await scrape(sequelize)
 })
