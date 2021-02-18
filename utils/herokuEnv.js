4ba770ba6bd03e0d461166d13b405d4eebbb737f
Rohin1212 <rohinchopra1212@gmail.com>
2021-02-18 14:00:41 +1100

    deployment: util to set heroku env vars

---
+++ utils/herokuEnv.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv')
+const { parsed } = dotenv.config({ path: './../server/.env' })
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
+async function setEnvVars(variables) {
+  for await (const variable of variables) {
+    const { stdout, stderr } = await exec(
+      `heroku config:set ${variable[0]}=${variable[1]}`
+    )
+    console.log('stdout:', stdout)
+    console.log('stderr:', stderr)
+  }
+}
+for (const envs in parsed) {
+  if (envs === 'DATABASE_URL') {
+    continue
+  }
+  setEnvVars([[envs, parsed[envs]]])
+}
