a0ba7f6ef07aad8592d158ae58b38530c814681d
Rohin1212 <rohinchopra1212@gmail.com>
2021-02-21 15:30:07 +1100

    decoupled scraping from server

--- server/server.js
+++ server/server.js
@@ -7,12 +7,9 @@ const sequelize = require('@Rohin1212/doubtfire-bot-sequelize')
 sequelize.start()
 
 const app = require('./app')
-const scrape = require('./../scraper')
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(blue(`listening on port ${port}`))
 })
-
-scrape(sequelize)
