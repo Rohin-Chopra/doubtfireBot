d09ab784935aae46507a660d0dd47cc1f7ba6fdf
Rohin1212 <rohinchopra1212@gmail.com>
2021-02-21 18:26:37 +1100

    added an http server to fix heroku idle port issue

--- scraper/index.js
+++ scraper/index.js
@@ -1,8 +1,16 @@
 require('dotenv').config({})
+const http = require('http')
 const cron = require('node-cron')
 const sequelize = require('@Rohin1212/doubtfire-bot-sequelize')
 const scrape = require('./scrape')
 const { yellow } = require('chalk')
+
+http
+  .createServer(function (req, res) {
+    res.write('Hello World!')
+    res.end()
+  })
+  .listen(process.env.PORT || 5000)
 
 scrape(sequelize)
 
