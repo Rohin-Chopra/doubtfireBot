7fc22bb5d90c3c6f3865324c192f8bc904d4d0fb
Rohin1212 <rohinchopra1212@gmail.com>
2021-02-24 21:31:11 +1100

    changed config to only use database url

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -2,26 +2,15 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    logging: false
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres'
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres'
   }
 }
