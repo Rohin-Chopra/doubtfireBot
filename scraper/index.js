b143e88ff9f63eba8237b50c26d951fbcede1532
Rohin1212 <rohinchopra1212@gmail.com>
2021-02-24 23:23:28 +1100

    shifted cron job to heroku scheduler

--- scraper/index.js
+++ scraper/index.js
@@ -1,23 +1,10 @@
 require('dotenv').config({})
-const http = require('http')
-const cron = require('node-cron')
 const sequelize = require('@Rohin1212/doubtfire-bot-sequelize')
 
 const scrape = require('./scrape')
 const { yellow } = require('chalk')
 
-http
-  .createServer(function (req, res) {
-    res.write('Hello World!')
-    res.end()
-  })
-  .listen(process.env.PORT || 5000)
-
 sequelize.start().then(() => {
-  scrape(sequelize)
-})
-
-cron.schedule('*/15 * * * *', async () => {
   console.log(yellow('about to scrape'))
-  await scrape(sequelize)
+  scrape(sequelize)
 })
