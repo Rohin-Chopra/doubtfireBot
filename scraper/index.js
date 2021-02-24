3e8c14f95db0e3cd1dce18f284ef9f15d63f7d7f
Rohin1212 <rohinchopra1212@gmail.com>
2021-02-25 00:04:08 +1100

    added helper to check run

--- scraper/index.js
+++ scraper/index.js
@@ -1,10 +1,35 @@
 require('dotenv').config({})
+const fs = require('fs')
 const sequelize = require('@Rohin1212/doubtfire-bot-sequelize')
 
 const scrape = require('./scrape')
 const { yellow } = require('chalk')
 
-sequelize.start().then(() => {
-  console.log(yellow('about to scrape'))
-  scrape(sequelize)
-})
+let timesRun = 0
+
+if (!fs.existsSync('./times-run.txt')) {
+  fs.writeFile('./times-run.txt', String(timesRun), (err) => {
+    if (err) {
+      console.log(err)
+    }
+  })
+} else {
+  timesRun = fs.readFileSync('./times-run.txt', {
+    encoding: 'utf8',
+    flag: 'r'
+  })
+  fs.writeFile('./times-run.txt', String(Number(timesRun) + 1), (err) => {
+    if (err) {
+      console.log(err)
+    }
+  })
+  console.log()
+}
+// read file
+// yes update the content
+// no write the content
+
+// sequelize.start().then(() => {
+//   console.log(yellow('about to scrape'))
+//   scrape(sequelize)
+// })
