da0555341f646e9c147dc1a6a195a157f7171f04
Rohin1212 <rohinchopra1212@gmail.com>
2021-02-24 21:35:45 +1100

    fix sequelize not starting

--- scraper/index.js
+++ scraper/index.js
@@ -2,6 +2,7 @@ require('dotenv').config({})
 const http = require('http')
 const cron = require('node-cron')
 const sequelize = require('@Rohin1212/doubtfire-bot-sequelize')
+
 const scrape = require('./scrape')
 const { yellow } = require('chalk')
 
@@ -12,7 +13,9 @@ http
   })
   .listen(process.env.PORT || 5000)
 
-scrape(sequelize)
+sequelize.start().then(() => {
+  scrape(sequelize)
+})
 
 cron.schedule('*/15 * * * *', async () => {
   console.log(yellow('about to scrape'))
